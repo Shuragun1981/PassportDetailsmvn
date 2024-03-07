6d7540da30a7eab323bb3adf482cc8a26a51ddb4
Aleksandr Volik <Aleksandr_volik@mail.ru>
2024-03-07 16:42:42 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String surname;
+    public String phone;
+    public String patronymic;
+    public boolean subscription;
+    public FormDate birthday;
+}
